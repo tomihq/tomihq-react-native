e8c2dab8d6b29e9ae9a1b1452c817f2ebea2ff0b
= <hernandeztomas584@gmail.com>
2023-01-08 03:38:44 -0300

    add: StyleSheets to our elements in CounterScreen

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,26 +1,48 @@
 import { useState } from "react"
-import { Text, View, Button} from "react-native"
+import { Text, View, TouchableOpacity, StyleSheet} from "react-native"
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState<number>(10);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center'}}>
-        <Text style={{textAlign: 'center', fontSize: 35, top: -15}}>Counter: {counter} </Text>
-        <Button
+    <View style={styles.container}>
+        <Text style={styles.title}>Counter: {counter} </Text>
+        <TouchableOpacity
           onPress={() => setCounter(prev => prev - 1)}
-          title="Decrease"
-          color="#841584"
           accessibilityLabel="Decrease Counter"
-        />
+        >
+          <View style={styles.button}> 
+            <Text> Decrease Counter</Text> 
+          </View>
+        </TouchableOpacity>
+        
 
-        <Button
+        <TouchableOpacity
           onPress={() => setCounter(prev => prev + 1)}
-          title="Increase"
-          color="#841584"
           accessibilityLabel="Increase Counter"
-        />
+        >
+          <View style={styles.button}> 
+              <Text> Increase Counter </Text> 
+          </View>
+        </TouchableOpacity>
 
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 100
+  },
+  title: {
+    textAlign: 'center', 
+    fontSize: 35, 
+    top: -15
+  }
+})
