90347da32f19965e56a529997eccb600da34be53
= <hernandeztomas584@gmail.com>
2023-01-07 23:28:26 -0300

    add: first commit

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export const App = () => {
   return (
     <View>
-      <Text>Lucia es mi chini toda hermocha!!!</Text>
+      <Text>Hello from React Native!</Text>
     </View>
   )
 }
