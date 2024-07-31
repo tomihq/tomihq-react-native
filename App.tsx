5b1eb07a6e36219303e9751d4f4f8a94d3c3daa5
Tom <hernandeztomas584@gmail.com>
2024-07-31 20:53:22 -0300

    add: counter app

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
-import { CounterScreen } from './src/presentation/screens/CounterScreen'
 import { PaperProvider } from 'react-native-paper'
-import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen'
 
 import IonIcon from 'react-native-vector-icons/Ionicons'
 
@@ -14,7 +12,7 @@ const App = () => {
     }}
    >
      <SafeAreaView style={{flex: 1}}>
-      <CounterM3Screen/>
+     {/*  <CounterM3Screen/> */}
     </SafeAreaView>
    </PaperProvider>
   )
