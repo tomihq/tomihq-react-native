9d8f1558fa6d15dd871f6d4bd3b7d619cdec6116
Tom <hernandeztomas584@gmail.com>
2024-07-31 22:14:49 -0300

    add: absoluteFillObject

--- src/presentation/screens/PositionScreen.tsx
+++ src/presentation/screens/PositionScreen.tsx
@@ -5,9 +5,9 @@ import { Text } from 'react-native-paper'
 export const PositionScreen = () => {
   return (
     <View style={styles.container}>
+        <View style={styles.greenBox}/>
         <View style={styles.purpleBox}/>
         <View style={styles.orangeBox}/>
-        <View style={styles.greenBox}/>
     </View>
   )
 }
@@ -39,14 +39,9 @@ const styles = StyleSheet.create({
     
     },
     greenBox: {
-        width: 100,
-        height: 100,
         backgroundColor: 'green',
         borderWidth: 10,
         borderColor: 'white',
-        position: 'absolute',
-        bottom: 0,
-        right: 0
-
+        ...StyleSheet.absoluteFillObject
     }
 });
