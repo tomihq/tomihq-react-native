121e61c53ab8aa48536799aae63806ef32e05d89
Tom <hernandeztomas584@gmail.com>
2024-07-31 21:46:14 -0300

    add: dimensions with Dimensions.get

--- src/presentation/screens/DimensionScreen.tsx
+++ src/presentation/screens/DimensionScreen.tsx
@@ -1,23 +1,31 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
+
+const { width, height } = Dimensions.get('window');
 
 export const DimensionScreen = () => {
   return (
-    <View style={styles.container}>
-        <Text style={styles.purpleBox}> Hello World! </Text>
+    <View>
+        <View style={styles.container}>
+            <Text style={styles.purpleBox}> Hello World! </Text>
+        </View>
+
+        <Text>w: {width}, h: {height}</Text>
     </View>
+   
   )
 }
 
 
 const styles = StyleSheet.create({
     container: {
-        width: '100%',
+        width: 400,
         height: 300,
         backgroundColor: 'red'
     },
     purpleBox: {
         backgroundColor: '#5856D6',
-        height: '50%'
+        height: '50%',
+        width: '50%'
     }
 });
